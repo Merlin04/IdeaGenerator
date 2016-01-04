c7f38f327b9df09188ed02c0eefe05a9daaef603
Merlin04 <Merlin04@users.noreply.github.com>
2016-01-03 18:11:54 -0800

    Update ideagen.py

--- ideagen.py
+++ ideagen.py
@@ -1,4 +1,4 @@
-__author__ = 'benjamin'
+
 import random
 # Most words are from momswhothink.com
 nouns = ['bob', 'pizza', 'scratch', 'cat', 'python', 'programming language', 'bag', 'game', 'apple', 'arm', 'banana', 'bike', 'bird', 'book', 'chin', 'clam', 'class', 'clover', 'club', 'corn', 'crayon', 'crow', 'crown', 'crowd', 'crib', 'desk', 'dime', 'dirt', 'dress', 'fang', 'field', 'flag', 'flower', 'fog', 'game', 'heat', 'hill', 'home', 'horn', 'hose', 'joke', 'juice', 'kite', 'lake', 'maid', 'mask', 'mice', 'milk', 'mint', 'meal', 'meat', 'moon', 'mother', 'morning', 'name', 'nest', 'nose', 'pear', 'pen', 'pencil', 'plant', 'rain', 'river', 'road', 'rock', 'room', 'rose', 'seed', 'shape', 'shoe', 'shop', 'show', 'sink', 'snail', 'snake', 'snow', 'soda', 'sofa', 'star', 'step', 'stew', 'stove', 'straw', 'string', 'summer', 'swing', 'table', 'tank', 'team', 'tent', 'test', 'toes', 'tree', 'vest', 'water', 'wing', 'winter', 'woman', 'women']
